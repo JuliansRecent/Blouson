e97d6423d24739d5ceb0b63aadf7c2cf7c4e813b
JuliansRecent <juliansrecent@gmail.com>
2022-06-22 11:16:06 -0700

    Create WhatsHotDisplay.js with map for products.

--- src/Components/WhatsHot/WhatsHotDisplay.js
+++ src/Components/WhatsHot/WhatsHotDisplay.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+
 function WhatsHotDisplay(props) {
     return (
         <div>
-            <div>some words</div>
+            <h2>What's Hot</h2>
+            <ul>
+                {products.length > 0 ? (products.map((product, index) => (
+                    <Product 
+                    key={index}
+                    index={index}
+                    product={product}
+                    />
+                ))
+                ) : (
+                    <div>Nothing here yet!</div>
+                )}
+            </ul>
         </div>
     );
 }
